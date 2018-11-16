80451c8ff52623a918793e1a6b7d428456a356bd
AlexandrTolstov <alextav_85@mail.ru>
2018-11-16 21:59:19 +0600

    C

--- Modul_02/MainWindow.xaml.cs
+++ Modul_02/MainWindow.xaml.cs
@@ -24,5 +24,107 @@ namespace Modul_02
         {
             InitializeComponent();
         }
+
+        private void Button_a_Click(object sender, RoutedEventArgs e)
+        {
+            Ex_a Ex_a_Window = new Ex_a();
+            Ex_a_Window.Show();
+        }
+
+        private void Button_b_Click(object sender, RoutedEventArgs e)
+        {
+            Ex_b Ex_b_Window = new Ex_b();
+            Ex_b_Window.Show();
+        }
+
+        private void Button_c_Click(object sender, RoutedEventArgs e)
+        {
+            Ex_c Ex_c_Window = new Ex_c();
+            Ex_c_Window.Show();
+        }
+
+        private void Button_d_Click(object sender, RoutedEventArgs e)
+        {
+            Ex_d Ex_d_Window = new Ex_d();
+            Ex_d_Window.Show();
+        }
+
+        private void Button_e_Click(object sender, RoutedEventArgs e)
+        {
+            Ex_e Ex_e_Window = new Ex_e();
+            Ex_e_Window.Show();
+        }
+
+        private void Button_f_Click(object sender, RoutedEventArgs e)
+        {
+            Ex_f Ex_f_Window = new Ex_f();
+            Ex_f_Window.Show();
+        }
+
+        private void Button_g_Click(object sender, RoutedEventArgs e)
+        {
+            Ex_g Ex_g_Window = new Ex_g();
+            Ex_g_Window.Show();
+        }
+
+        private void Button_h_Click(object sender, RoutedEventArgs e)
+        {
+            Ex_h Ex_h_Window = new Ex_h();
+            Ex_h_Window.Show();
+        }
+
+        private void Button_i_Click(object sender, RoutedEventArgs e)
+        {
+            Ex_i Ex_i_Window = new Ex_i();
+            Ex_i_Window.Show();
+        }
+
+        private void Button_j_Click(object sender, RoutedEventArgs e)
+        {
+            Ex_j Ex_j_Window = new Ex_j();
+            Ex_j_Window.Show();
+        }
+
+        private void Button_k_Click(object sender, RoutedEventArgs e)
+        {
+            Ex_k Ex_k_Window = new Ex_k();
+            Ex_k_Window.Show();
+        }
+
+        private void Button_l_Click(object sender, RoutedEventArgs e)
+        {
+            Ex_l Ex_l_Window = new Ex_l();
+            Ex_l_Window.Show();
+        }
+
+        private void Button_m_Click(object sender, RoutedEventArgs e)
+        {
+            Ex_m Ex_m_Window = new Ex_m();
+            Ex_m_Window.Show();
+        }
+
+        private void Button_n_Click(object sender, RoutedEventArgs e)
+        {
+            Ex_n Ex_n_Window = new Ex_n();
+            Ex_n_Window.Show();
+        }
+
+        private void Button_o_Click(object sender, RoutedEventArgs e)
+        {
+            Ex_o Ex_o_Window = new Ex_o();
+            Ex_o_Window.Show();
+        }
+
+        private void Button_p_Click(object sender, RoutedEventArgs e)
+        {
+            Ex_p Ex_p_Window = new Ex_p();
+            Ex_p_Window.Show();
+        }
+
+        private void Button_q_Click(object sender, RoutedEventArgs e)
+        {
+            Ex_q Ex_q_Window = new Ex_q();
+            Ex_q_Window.Show();
+        }
     }
 }
